0d1b68ab153cbab49b651817245f8f04cbd8f76c
Jaderson R. Ilidio <78319352+Jadersonrilidio@users.noreply.github.com>
2022-01-18 17:06:24 -0300

    Update Config.php

--- classes/Config.php
+++ classes/Config.php
@@ -2,6 +2,12 @@
 
 abstract class Config {
     
+    // smpt configuration variables
+    const SMPT_HOST = 'localhost';                      
+    const SMTP_USER = 'email@example.com';                     
+    const SMTP_PASSWORD = '**********';          
+    const SMPT_PORT = '80';
+    
     // Path constants
     const REL_PATH = '/';
     const ADMIN_REL_PATH = '/admin/';
